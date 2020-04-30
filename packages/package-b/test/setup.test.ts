3c9211bb67572f252952cfb6e4ccc6da1322b878
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-04-30 22:42:53 +0200

    move dependencies into main package.json

--- packages/package-b/test/setup.test.ts
+++ packages/package-b/test/setup.test.ts
@@ -1,10 +1,8 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import sinonChai from 'sinon-chai'
-import chaiHttp from 'chai-http'
 import chaiSubset from 'chai-subset'
+import sinonChai from 'sinon-chai'
 
 chai.use(chaiAsPromised)
 chai.use(sinonChai)
-chai.use(chaiHttp)
 chai.use(chaiSubset)
